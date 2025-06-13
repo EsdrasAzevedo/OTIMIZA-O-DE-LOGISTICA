0d41572906f77a02d9f1ce0fead326c501257934
EsdrasAzevedo <135185989+EsdrasAzevedo@users.noreply.github.com>
2025-06-12 22:23:08 -0300

    mudança no roteamento

--- A3 - API EM PYTHON/roteamento.py
+++ A3 - API EM PYTHON/roteamento.py
@@ -1,33 +1,27 @@
 import networkx as nx  # Usado para acessar Dijkstra e manipular o grafo
 
-# Função que determina o centro de distribuição mais próximo de um destino
-
 
 def centro_mais_proximo(entrega, centros, grafo):
-    menor_distancia = float('inf')  # Inicializa com infinito
+    menor_distancia = float('inf')
     centro_mais_proximo = None
 
     for centro in centros:
+        if centro == entrega['destino']:
+            continue  
+
         try:
-            # Calcula a menor distância do centro até o destino usando Dijkstra
             distancia = nx.dijkstra_path_length(
                 grafo, centro, entrega['destino'])
-
-            # Se a distância for menor que a menor registrada, atualiza
             if distancia < menor_distancia:
                 menor_distancia = distancia
                 centro_mais_proximo = centro
         except nx.NetworkXNoPath:
-            # Caso não exista caminho entre o centro e o destino
             continue
 
     return centro_mais_proximo
 
-# Função que retorna a rota e a distância entre origem e destino
-
 
 def calcular_rota(grafo, origem, destino):
-    rota = nx.dijkstra_path(grafo, origem, destino)  # Caminho mais curto
-    distancia = nx.dijkstra_path_length(
-        grafo, origem, destino)  # Distância total
+    rota = nx.dijkstra_path(grafo, origem, destino)
+    distancia = nx.dijkstra_path_length(grafo, origem, destino)
     return rota, distancia
