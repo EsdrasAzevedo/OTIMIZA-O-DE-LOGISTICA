1bf85039b6994fb8d01a28d0cf6a3bc6f056f4fa
EsdrasAzevedo <135185989+EsdrasAzevedo@users.noreply.github.com>
2025-06-12 22:23:40 -0300

    mudança no main

--- A3 - API EM PYTHON/main.py
+++ A3 - API EM PYTHON/main.py
@@ -1,6 +1,5 @@
 from cenarios_teste import testar_cenarios
 
-# Este é o arquivo principal que inicia o sistema
 if __name__ == "__main__":
     print("===== SISTEMA DE ROTEAMENTO LOGÍSTICO =====")
     testar_cenarios()
